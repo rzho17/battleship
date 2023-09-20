dce129990ee36d28de173a20323b68df135371cc
Richard <richardzehungho@gmail.com>
2023-09-20 15:31:29 -0700

    Add initial layout screen

--- src/domFunctions.js
+++ src/domFunctions.js
@@ -1,12 +1,52 @@
 const makeGrid = (board) => {
+  let counter = 0;
   for (let i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
       const gridSquare = document.createElement("div");
       board.appendChild(gridSquare);
       gridSquare.className = "gridSquare";
-      gridSquare.dataset.num = i + j;
+      gridSquare.dataset.gridPosition = `${counter}${j}`;
     }
+    counter++;
   }
 };
+
+const initialLoad = () => {
+  const main = document.querySelector("main");
+  const loadContainer = document.createElement("div");
+  loadContainer.className = "loadContainer";
+
+  const loadInfo = document.createElement("div");
+  loadInfo.className = "loadInfo";
+
+  const loadTitle = document.createElement("h2");
+  loadTitle.className = "loadTitle";
+  loadTitle.innerText = "Let's play Battleship!";
+
+  const displayShip = document.createElement("div");
+  displayShip.className = "displayShip";
+  displayShip.innerText = "Carrier";
+
+  const rotateBtn = document.createElement("button");
+  rotateBtn.className = "rotateBtn";
+  rotateBtn.innerText = "Rotate Ship";
+
+  const initialBoard = document.createElement("div");
+  initialBoard.className = "gameBoard initialBoard";
+
+  loadInfo.append(loadTitle, displayShip, rotateBtn, initialBoard);
+
+  loadContainer.append(loadInfo);
+
+  makeGrid(initialBoard);
+
+  //   loadContainer.innerText = "asdfasdf";
+
+  main.append(loadContainer);
+
+  //   main.innerHTML = "";
+};
+
+initialLoad();
 
 export default makeGrid;
